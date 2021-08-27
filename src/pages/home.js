840515a2c7529fdee9d5448102aabdf2b1606709
rizki <rizki.delameta19@gmail.com>
2021-08-27 10:50:35 +0700

    add handle buttonBack

--- src/pages/home.js
+++ src/pages/home.js
@@ -4,10 +4,23 @@ const home = () => {
   const animeListSideArr = document.querySelectorAll('anime-list-side');
   const animeList = [...animeListContentArr, ...animeListSideArr];
 
+  const buttonBackEl = () => {
+    const buttonBack = document.createElement('button');
+    const textButtonBack = document.createTextNode('Back');
+    buttonBack.appendChild(textButtonBack);
+    buttonBack.addEventListener('click', () => {
+      contentElement.innerHTML = `
+        <anime-list-content category="Summer 2021 Anime"></anime-list-content>
+        <anime-list-content category="Upcoming Anime"></anime-list-content>`;
+    });
+    return buttonBack;
+  };
+
   const handleClickMore = async (title) => {
     contentElement.innerHTML = '<loading-data/>';
     setTimeout(() => {
-      contentElement.innerHTML = `<anime-list-content category="${title}" isMore="true"></anime-list-content>`;
+      contentElement.innerHTML = `<anime-list-content category="${title}" isMore="false"></anime-list-content>`;
+      contentElement.appendChild(buttonBackEl());
     }, 2000);
   };
 
